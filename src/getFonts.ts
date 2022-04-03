3cf0940e77ef948cb77e85abe5621451e72fa288
Khant Kyaw Moe <khantkyawmoe.kkm@gmail.com>
2022-04-03 19:42:42 +0630

    Update getFonts.ts

--- src/getFonts.ts
+++ src/getFonts.ts
@@ -3,7 +3,7 @@ import { Font } from './types'
 
 export function getFonts() {
   if (!fonts) {
-    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ၀, ၁, ၂, ၃, ၄, ၅, ၆, ၇, ၈, ၉]
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '၀', '၁', '၂', '၃', '၄', '၅', '၆', '၇', '၈', '၉']
     const lower = [
       'a',
       'b',
